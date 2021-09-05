920eb723746131704452f140532490f75ad90ffe
Robert <giuffredarobert@gmail.com>
2021-09-05 19:38:42 -0400

    Glad and glfw code added. Still initial commits

--- Volume/src/main.cpp
+++ Volume/src/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+const unsigned int SCR_WIDTH = 720;
+const unsigned int SCR_HEIGHT = 480;
 
 int main(void)
 {
+	GLFWwindow* window;
+
+	if (!glfwInit())
+	{
+		std::cout << "GLFW initialization failed!" << std::endl;
+		return -1;
+	}
+	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Volume Render", nullptr, nullptr);
+	if (!window)
+	{
+		std::cout << "Window/OpenGL context creation failed." << std::endl;
+		glfwTerminate();
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize OpenGL context" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap the front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
